9c00d8d8df05c3a646dcd0a3d2b62eb73f9282a6
deram <deram@iki.fi>
2013-01-19 02:43:59 +0200

    svg data to code textarea (should be hidden in final version) for submitting to server

--- editor.js
+++ editor.js
@@ -3,6 +3,8 @@ var SVG_HEAD;
 var SVG_TEXT;
 var INPUT_TEXT;
 var INPUT_HEAD;
+var CODE;
+var SERIALIZE;
 
 var svgNS = "http://www.w3.org/2000/svg";
 var xmlNS = "http://www.w3.org/XML/1998/namespace";
@@ -14,6 +16,9 @@ function prepare(){
   INPUT_HEAD=document.getElementById("head");
   INPUT_HEAD.addEventListener('input', update, false);
   INPUT_HEAD.wrap='off';
+  CODE=document.getElementById("code");
+
+  SERIALIZE = new XMLSerializer();
 
   var S=document.getElementById("svg");
   try{SVG=S.contentDocument}
@@ -29,23 +34,46 @@ function clear_element(element) {
   }
 }
 
+function create_emphasis_tspan(text) {
+  var tspan = document.createElementNS(svgNS, "tspan");
+  tspan.appendChild(SVG.createTextNode(text));
+  tspan.setAttributeNS(null, "fill", "#FF5500");
+  return tspan;
+}
+
+function create_line_tspan(text) {
+  var array=text.split(/<([^>]*)>/g);
+  var tspan = document.createElementNS(svgNS, "tspan");
+  tspan.setAttributeNS(xmlNS, "xml:space", "preserve");
+  for (var i in array) {
+    if (i%2) {
+      if (array[i]) tspan.appendChild(create_emphasis_tspan(array[i]));
+    } else {
+      if (array[i]) tspan.appendChild(SVG.createTextNode(array[i]));
+    }
+  }
+  tspan.appendChild(SVG.createTextNode(" "));
+  return tspan;
+}
+
 function set_multiline_text(output, input) {
-  var array=input.value.split(/\r\n|\n|\r/);
+  var linearray=input.value.split(/\r\n|\n|\r/);
   clear_element(output);
   var x=output.getAttributeNS(null, "x")
-  for (i in array) {
-    var tspan = document.createElementNS(svgNS, "tspan");
+  for (var i in linearray) {
+    var tspan = create_line_tspan(linearray[i]);
     if (i > 0) tspan.setAttributeNS(null, "dy", "1em");
     tspan.setAttributeNS(null, "x", x);
-    tspan.setAttributeNS(xmlNS, "xml:space", "preserve");
-    tspan.appendChild(SVG.createTextNode(array[i] + " "));
     output.appendChild(tspan);
   }
 }
 
+
+
 function update(){
   set_multiline_text(SVG_HEAD, INPUT_HEAD);
   set_multiline_text(SVG_TEXT, INPUT_TEXT);
+  CODE.value=SERIALIZE.serializeToString(SVG);
 }
 
 function onload() {
